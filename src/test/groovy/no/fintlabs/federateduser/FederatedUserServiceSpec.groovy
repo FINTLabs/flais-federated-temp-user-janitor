b8872b2d3dfcdd3809e34f3f0f05c8713ab949b8
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-03-26 21:17:46 +0200

    Added tests

---
+++ src/test/groovy/no/fintlabs/federateduser/FederatedUserServiceSpec.groovy
@@ -0,0 +1,24 @@
+package no.fintlabs.federateduser
+
+import groovy.util.logging.Slf4j
+import spock.lang.Specification
+
+@Slf4j
+class FederatedUserServiceSpec extends Specification {
+
+    private FederatedUserRepository federatedUserRepository = Mock(FederatedUserRepository)
+
+    def "deleteAllFederatedUsers should remove all users"() {
+        given:
+        def federatedUserService = new FederatedUserService(federatedUserRepository)
+
+        when:
+        federatedUserService.deleteAllFederatedUsers()
+
+        then:
+        federatedUserService.getFederatedUsers().size() == 2
+        federatedUserService.getDeletedFederatedUsers().size() == 2
+        1 * federatedUserRepository.findAll() >> [new FederatedUser(), new FederatedUser()]
+        2 * federatedUserRepository.delete(_ as FederatedUser)
+    }
+}
